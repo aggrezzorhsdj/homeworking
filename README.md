729330a731621ffb66941f921263b73c80419bf7
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2019-11-05 23:15:18 +0300

    adding tutor on try catch

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Homeworking repo for Netcracker Education
 <ol>
     <li>Let's see the dropdown - <a href="https://aggrezzorhsdj.github.io/homeworking/dropdown/">click</a></li>
     <li>Let's see the objects - <a href="https://aggrezzorhsdj.github.io/homeworking/objects/index.html">click</a></li>
+    <li>Let's see the try {} catch(){} - <a href="https://aggrezzorhsdj.github.io/homeworking/trycatch/index.html">click</a></li>
 </ol>
---
+++ trycatch/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+function createTryCatch(selector,initiator){
+    selector = document.querySelector(selector);
+    function CreatorMoodOfVasya(mood) {
+        this.name = 'Vasya';
+        this.surname = 'Pupkin';
+        this.mood = mood;
+        Object.defineProperty(this,"itsVasilii",{
+            get: function(){
+                try {
+                    if(mood == 1){
+                        return JSON.parse(this.name + ' ' + this.surname+ ' '+ this.mood);
+                    }else{
+                        return JSON.parse('{"name":"'+this.name+'","surname":"'+this.surname+'"}');
+                    }
+
+                } catch(e){
+                    return 'Ошибка ' + e.name + ":" + e.message + "\n" + e.stack;
+                }
+            }
+        });
+    }
+    initiator = document.getElementById(initiator);
+    var result = document.getElementById('result');
+    if(initiator !==  null){
+        initiator.addEventListener('click',function(){
+            var v = new CreatorMoodOfVasya(selector.value);
+            console.log(v.itsVasilii);
+            result.innerHTML = v.itsVasilii;
+        });
+    }
+}
+createTryCatch('#errorType','creator');
