7e77b8690ea6b1afa962b0782de07dc76caf8c14
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2019-11-11 15:33:46 +0300

    update

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Homeworking repo for Netcracker Education
     <li>Let's see the dropdown - <a href="https://aggrezzorhsdj.github.io/homeworking/dropdown/">click</a></li>
     <li>Let's see the objects - <a href="https://aggrezzorhsdj.github.io/homeworking/objects/index.html">click</a></li>
     <li>Let's see the try {} catch(){} - <a href="https://aggrezzorhsdj.github.io/homeworking/trycatch/index.html">click</a></li>
+    <li>Let's see the classes - <a href="https://aggrezzorhsdj.github.io/homeworking/classes/index.html">click</a></li>
 </ol>
---
+++ classes/js/main.js
@@ -0,0 +1,53 @@
+function Human() {}
+Human.prototype.init = function(fullName) {
+    this.fullName = fullName;
+};
+Human.prototype.info = function(age,weight,height){
+    this.age = age;
+    this.weight = weight;
+    this.height = height;
+}
+Human.prototype.print = function() {
+    console.log(this.fullName);
+};
+
+
+function inherit(ParentClass) {
+
+    function ChildClass() {}
+    ChildClass.prototype = Object.create(ParentClass.prototype);
+    ChildClass.prototype.constructor = ChildClass;
+    ChildClass.prototype._super = ParentClass.prototype;
+
+    return ChildClass;
+}
+
+
+var Man = inherit(Human);
+var Woman = inherit(Human);
+
+Man.prototype.init = function(fullName) {
+    fullName = 'Dorogoy ' + fullName;
+    this._super.init.call(this, fullName); // call super
+}
+Man.prototype.print = function(){
+    console.log(this.fullName+' '+' he is Man');
+}
+
+Woman.prototype.init = function(fullName) {
+    fullName = 'Dorogaya ' + fullName;
+    this._super.init.call(this, fullName); // call super
+}
+Woman.prototype.print = function(){
+    console.log(this.fullName+' '+' she is Woman');
+}
+
+var man = new Man();
+man.init('Vasili ivanovich');
+man.print();
+
+var woman = new Woman();
+woman.init('Vasili ivanovich');
+woman.print();
+
+
